23dd128adbfb9b3c0440ee6b9ca1f4002d97ce4c
Keita Ohshiro <keita.ohshiro@gmail.com>
2021-02-01 15:47:10 -0500

    added sound

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ const width = 500;
 const height = 500;
 const gridSize = 25;
 
+let osc, playing, freq, amp; // for oscillator
+
+let pitches = [];
+
 function setup() {
   createCanvas(width,height);
   background(200);
@@ -9,8 +13,12 @@ function setup() {
 
   noFill();
 
-  //drawLinearEquation();
-  drawQuadraticEquation();
+  osc = new p5.Oscillator('sine');
+  osc.amp(0.5);
+  playing = false;
+
+  drawLinearEquation();
+  //drawQuadraticEquation();
 }
 
 function draw() {
@@ -47,13 +55,17 @@ function drawUnits() {
 function drawLinearEquation() {
   //y=x
   strokeWeight(1);
-  translate(width/2, height/2);
+  translate(width/2, height/2); // might be better not use translate
   slope = 1;
   beginShape();
   for (var x = -width/2; x <=width/2; x++) {
     y = -(slope*x);
-    vertex(x, y);
-  }  
+    vertex(x*gridSize, y*gridSize);
+    pitch = map(-y, -250, 250, 40, 880);
+    i = x + 250; // 250 for make array index positive
+    pitches[i] = pitch;
+  }
+  print(pitches);
   endShape();
 }
 
@@ -68,4 +80,27 @@ function drawQuadraticEquation() {
     vertex(x*gridSize, y*gridSize);
   }
   endShape();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    if (playing === false) {
+      print("play");
+      osc.start();
+      playing = true;
+      for (var i = 0; i < 502; i++) {
+        osc.freq(pitches[i]);
+        sleep(10);
+      }
+    } else {
+      osc.stop();
+      playing = false;
+    }
+  }
+}
+
+// https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep/39914235#39914235
+function sleep(milliseconds){
+    var waitUntil = new Date().getTime() + milliseconds;
+    while(new Date().getTime() < waitUntil) true;
 }
